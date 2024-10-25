b2ae94d2590bf796b951e32bce98b3f7126c4cbd
Qihan Wang <97229036+HenryBlake@users.noreply.github.com>
2024-10-24 22:52:56 -0400

    Create the Header Componet and base structure

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "../Header/HeaderStyle.css"
+function Header(){
+    return(
+        <header>
+            <p className="Title">Henry(Qihan) Wang</p>
+            <p className="LinkedIn">LinkedIn</p>
+            <p className="GitHub">GitHub</p>
+            <button id="contact_me_btn">Contact Me</button>
+        </header>
+    )
+}
+export default Header
